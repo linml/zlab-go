9d97619fec22953bb8a9e73d38b4dfeb78e1540e
yixinin <yixinin99@gmail.com>
2019-05-09 18:50:05 +0800

    update.

--- core/player/player.go
+++ core/player/player.go
@@ -1,9 +1,15 @@
 package player
 
 type Player struct {
-	ID       string
-	NickName string
-	Avatar   string
-	OpenID   string
-	UnionID  string
+	ID        string
+	NickName  string
+	Avatar    string
+	OpenID    string
+	UnionID   string
+	GameLevel uint8 //游戏角色类型 机器人 代理 普通玩家
+
+	Money int64 //金币数量
+
+	RoomID string //当前所在房间
+	State  uint8  //玩家状态
 }
